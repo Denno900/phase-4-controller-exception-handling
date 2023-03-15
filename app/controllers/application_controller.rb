de4ddc7a420d65dbe484b13da789a05f0ce33ae1
dennis mwaniki <dennomaz@gmail.com>
2023-03-16 01:42:10 +0300

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,17 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+  private
+
+  
+
+  def render_not_found_response
+    @model_name = controller_name.classify
+  puts "--------"
+  puts @model_name
+  puts "--------"
+    render json: { error: "#{@model_name} not found" }, status: :not_found
+  end
 end
